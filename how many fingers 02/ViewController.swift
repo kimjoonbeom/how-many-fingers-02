558e0247e118b92fa9733448380fd9201fd5a3be
D7703_16 <d7703_16@D7703-16ui-iMac.local>
2018-03-26 14:42:56 +0900

    how many fingers 02

--- how many fingers 02/ViewController.swift
+++ how many fingers 02/ViewController.swift
@@ -10,16 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var input: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+
+    
+    
+    @IBAction func guess(_ sender: Any) {
+        //랜덤 숫자 생성
+        let number = arc4random() % 6
+        print(number)
+    let numberString = String(number)
+        if input.text == numberString {
+            resultLabel.text = "빙고!!!!"
+        } else {
+    resultLabel.text = "실패!!! try again!! 숨겨진숫자는 현재\(number)"
+        }
+        
+    }
+    
     }
 
 
-}
 
